91e8b465e4c19819e28717f4bf22acb7ed341b84
Raphael Pansi <raphael.pansi@datenkraft.com>
2021-04-21 11:20:03 +0200

    BB-127 Relocate OAuth2 authorization to Auth class, create config for oAuthTokenUrl

--- tests/Auth/AuthTest.php
+++ tests/Auth/AuthTest.php
@@ -1,20 +1,17 @@
 <?php
 
-namespace BBBaseApiPhpClient;
+namespace BBBaseApiPhpClient\Auth;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\RequestOptions;
 use PHPUnit\Framework\TestCase;
 
-class BaseClientTest extends TestCase
+class AuthTest extends TestCase
 {
-    protected BaseClient $client;
 
     protected function setUp(): void
     {
         parent::setUp();
-
-        $this->client = new BaseClient();
     }
 
     protected function tearDown(): void
@@ -23,17 +20,17 @@ class BaseClientTest extends TestCase
     }
 
     // Test OAuth2 authorization with thephpleague/oauth2-client
-    // This test only serves as a demo for retrieving the token from a local instance of the auth server
+    // This only serves as a demo for retrieving the token from a local instance of the auth server
     // It has to be adapted for using it in production/staging
     public function testAuthorize(): void
     {
-        // Set the url of the auth server (local instance running in the same docker network)
-        $this->client->setOAuthServerUrl('https://bb_authorization_api:3000');
+        // Url of the auth server (local instance running in the same docker network)
+        $oAuthTokenUrl = 'https://bb_authorization_api:3000/oauth/token';
 
-        // Set a valid clientId, clientSecret and the requested scopes
-        $this->client->setClientId('933daa6d-90da-44d5-8f2b-13f97fb2659c');
-        $this->client->setClientSecret('8q6wAYC6mhomMA1SP2xjpykKSqGrGugfuitKerlI');
-        $this->client->setOAuthScopes([]);
+        // Valid clientId, clientSecret and requested scopes
+        $clientId = '933daa6d-90da-44d5-8f2b-13f97fb2659c';
+        $clientSecret = '8q6wAYC6mhomMA1SP2xjpykKSqGrGugfuitKerlI';
+        $oAuthScopes = [];
 
         // Disable SSL certificate validation (local auth server uses self-signed certificates)
         $guzzleClient = new Client(
@@ -43,15 +40,14 @@ class BaseClientTest extends TestCase
         );
 
         // Authorize the client
-        $this->client->authorize($guzzleClient);
-        $token = $this->client->getAccessToken();
+        $token = Auth::authorize($clientId, $clientSecret, $oAuthScopes, $guzzleClient, $oAuthTokenUrl);
 
         // Decode header and payload of the JWT token and make assertions
         $header = json_decode(base64_decode(explode('.', $token)[0]), true);
         $payload = json_decode(base64_decode(explode('.', $token)[1]), true);
 
         $this->assertEquals('JWT', $header['typ']);
-        $this->assertEquals($this->client->getClientId(), $payload['aud']);
-        $this->assertEquals($this->client->getOAuthScopes(), $payload['scopes']);
+        $this->assertEquals($clientId, $payload['aud']);
+        $this->assertEquals($oAuthScopes, $payload['scopes']);
     }
 }
