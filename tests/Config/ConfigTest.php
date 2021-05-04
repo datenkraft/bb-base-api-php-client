dd90c71e9997c70343061cc70abe1387c5e04012
Andreas <andreas.uretschnig@datenkraft.com>
2021-05-04 20:48:24 +0200

    BB-7: review changes (formatting + tests)

--- tests/Config/ConfigTest.php
+++ tests/Config/ConfigTest.php
@@ -21,15 +21,30 @@ class ConfigTest extends TestCase
      * @dataProvider createDataProvider
      * @covers ::create
      * @covers ::verifyConfigOptions
+     * @covers ::initByConfigOptions
      */
     public function testCreate(bool $valid, array $configOptions): void
     {
         if (!$valid) {
             $this->expectException(ConfigException::class);
         }
+        $configOptions = array_merge(require(__DIR__ . '/../../config/config.php'), $configOptions);
         $config = Config::create($configOptions);
         $this->assertInstanceOf(Config::class, $config);
+
         $this->assertSame($configOptions['clientId'], $config->getClientId());
+        $this->assertSame($configOptions['clientSecret'], $config->getClientSecret());
+        $this->assertSame($configOptions['oAuthScopes'], $config->getOAuthScopes());
+
+        if (!isset($configOptions['oAuthTokenUrl'])) {
+            $configOptions['oAuthTokenUrl'] = null;
+        }
+        if (!isset($configOptions['verifySsl'])) {
+            $configOptions['verifySsl'] = null;
+        }
+
+        $this->assertSame($configOptions['oAuthTokenUrl'], $config->getOAuthTokenUrl());
+        $this->assertSame($configOptions['verifySsl'], $config->getVerifySsl());
     }
 
     /**
@@ -38,6 +53,16 @@ class ConfigTest extends TestCase
     public function createDataProvider(): array
     {
         return [
+            [
+                true,
+                [
+                    'clientId' => 'clientId',
+                    'clientSecret' => 'clientSecret',
+                    'oAuthScopes' => ['oAuthScopes'],
+                    'oAuthTokenUrl' => 'oAuthTokenUrl',
+                    'verifySsl' => 'verifySsl',
+                ]
+            ],
             [
                 true,
                 [
