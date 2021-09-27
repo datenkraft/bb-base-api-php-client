9457fade7714c6a5f69933a8ff133f44003ca35f
Raphael Pansi <raphael.pansi@datenkraft.com>
2021-09-27 13:06:18 +0200

    BB-1294 Review

--- config/config.php
+++ config/config.php
@@ -3,6 +3,6 @@
 return array(
     'oAuthScopes' => [],
     'oAuthTokenUrl' => getenv('X_DATENKRAFT_OAUTH_TOKEN_URL') ?:
-        'https://authorization-api.conqore.niceshops.com/oauth/token',
+        'https://authentication-api.conqore.niceshops.com/oauth/token',
     'verifySsl' => !(getenv('X_DATENKRAFT_VERIFY_SSL') === 'false'),
 );
